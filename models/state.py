f7ce0694d8871824d64280cc646cd3e86037d711
edreez <olawunmiolatunji77@gmail.com>
2022-08-08 03:53:55 +0000

    state

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Class state that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """class to create a state"""
+    name = ""
