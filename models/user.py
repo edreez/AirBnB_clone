bb7a94218cb9f89ef5fb6a4098216e96e8c0da5e
edreez <olawunmiolatunji77@gmail.com>
2022-08-08 03:35:35 +0000

    inherit from bassemodel

---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A class User that inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """User creation"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
